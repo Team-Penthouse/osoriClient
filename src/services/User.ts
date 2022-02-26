5beb166cc7fe1c5c3563efbdbaff881f7ed9dd0b
bongbong2da <snk3981@gmail.com>
2022-02-27 01:04:58 +0900

    [Add] Typescript API Files

---
+++ src/services/User.ts
@@ -0,0 +1,81 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+import { HttpClient, RequestParams } from './http-client';
+
+export class User<SecurityDataType = unknown> extends HttpClient<SecurityDataType> {
+  /**
+   * No description
+   *
+   * @tags User
+   * @name UsersList
+   * @request GET:/users
+   */
+  usersList = (params: RequestParams = {}) =>
+    this.request<void, void>({
+      path: `/users`,
+      method: 'GET',
+      ...params,
+    });
+  /**
+   * No description
+   *
+   * @tags User
+   * @name UserCreate
+   * @request POST:/user
+   */
+  userCreate = (data: { nickname?: string; loginType?: string }, params: RequestParams = {}) =>
+    this.request<void, void>({
+      path: `/user`,
+      method: 'POST',
+      body: data,
+      ...params,
+    });
+  /**
+   * No description
+   *
+   * @tags User
+   * @name UserDetail
+   * @request GET:/user/{userId}
+   */
+  userDetail = (userId: number, params: RequestParams = {}) =>
+    this.request<void, void>({
+      path: `/user/${userId}`,
+      method: 'GET',
+      ...params,
+    });
+  /**
+   * No description
+   *
+   * @tags User
+   * @name UpdateUser
+   * @request UPDATE:/user/{userId}
+   */
+  updateUser = (userId: string, params: RequestParams = {}) =>
+    this.request<void, void>({
+      path: `/user/${userId}`,
+      method: 'UPDATE',
+      ...params,
+    });
+  /**
+   * No description
+   *
+   * @tags User
+   * @name UserDelete
+   * @request DELETE:/user/{userId}
+   */
+  userDelete = (userId: number, params: RequestParams = {}) =>
+    this.request<void, void>({
+      path: `/user/${userId}`,
+      method: 'DELETE',
+      ...params,
+    });
+}
---
+++ src/services/data-contracts.ts
@@ -0,0 +1,48 @@
+/* eslint-disable */
+/* tslint:disable */
+/*
+ * ---------------------------------------------------------------
+ * ## THIS FILE WAS GENERATED VIA SWAGGER-TYPESCRIPT-API        ##
+ * ##                                                           ##
+ * ## AUTHOR: acacode                                           ##
+ * ## SOURCE: https://github.com/acacode/swagger-typescript-api ##
+ * ---------------------------------------------------------------
+ */
+
+export interface UserDto {
+  id?: number;
+  loginType: 'NONE' | 'KAKAO' | 'GOOGLE' | 'APPLE';
+  externalId?: number;
+  nickname: string;
+  gender?: string;
+  email?: string;
+  name?: string;
+  phoneNumber?: string;
+  profileImg?: number;
+  ageRange?: string;
+  birthday?: string;
+
+  /** @format date */
+  createDate?: string;
+
+  /** @format date */
+  modifyDate?: string;
+}
+
+export interface ArticleDto {
+  id?: number;
+  creatorId: number;
+  categoryId?: number;
+  title: string;
+  viewCount?: number;
+  likeCount?: number;
+  tags?: string;
+  contents: string;
+  isPublished?: boolean;
+
+  /** @format date */
+  createDate?: string;
+
+  /** @format date */
+  modifyDate?: string;
+}
