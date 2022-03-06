8a1c7dd1b3b0fa8c34ed234e51f04333d2bb0ff6
bongbong2da <snk3981@gmail.com>
2022-03-06 17:55:02 +0900

    [Modify] Apply Animation To LoginScreen.tsx

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Alert } from 'react-native';
+import React, { useRef } from 'react';
+import { Alert, Animated } from 'react-native';
 import { getProfile, KakaoProfile, login } from '@react-native-seoul/kakao-login';
 import { isUndefined } from 'lodash';
 import LottieView from 'lottie-react-native';
@@ -7,25 +7,19 @@ import { useMutation } from 'react-query';
 import { observer } from 'mobx-react';
 import styled from 'styled-components/native';
 import Text from 'components/Text';
-import { DEVICE_WIDTH } from 'layout/CustomStyles';
-import { User } from 'services/User';
+import { DEVICE_HEIGHT, DEVICE_WIDTH } from 'layout/CustomStyles';
 import { useStore } from 'stores/RootStore';
+import { loginByKakao } from '../services/Kakao';
 
 const LoginScreen = observer(() => {
   const { userStore } = useStore();
-  // const navigation = useNavigation<StackNavigationProp<any>>();
-  /**
-   * @deprecated
-   */
 
-  /**
-   * @States
-   */
+  const introContainerOpacity = useRef(new Animated.Value(1)).current;
+  const welcomeContainerOpacity = useRef(new Animated.Value(0)).current;
 
   /**
    * @Variables
    */
-  const api = new User();
 
   /**
    * @Queries
@@ -38,7 +32,7 @@ const LoginScreen = observer(() => {
         externalId: user?.id,
         profileImg: user?.profileImageUrl,
       };
-      return await api.userCreate(body);
+      return await userStore.api.userCreate(body);
     },
     {
       onSuccess: (result: any) => {
@@ -47,64 +41,83 @@ const LoginScreen = observer(() => {
     },
   );
 
-  const getUserByExternalId = async (user: KakaoProfile): Promise<any> => {
-    await api
+  const getUserByExternalId = async (user: KakaoProfile) => {
+    await userStore.api
       .userDetail(Number(user.id), { loginType: 'KAKAO' } as any)
       .then(async (result: any) => {
         if (result?.data === 'USER_NOT_FOUND') {
           await saveUser.mutate(user);
         }
-        userStore.setIsLoggedIn(true);
-        console.log('login', userStore.isLoggedIn);
+        moveToMainScreenWithAnimation();
       });
   };
 
   const handleLoginByKakao = async () => {
-    await login()
-      .then(async (res) => {
-        if (!isUndefined(res.accessToken)) {
-          // @ts-ignore
-          const userInfo: KakaoProfile = await getProfile();
-          if (typeof userInfo.nickname !== 'undefined') {
-            await getUserByExternalId(userInfo);
-          }
-        } else {
-          Alert.alert('잘못된 로그인입니다.');
+    await loginByKakao({
+      onProvided: async (userInfo) => {
+        if (typeof userInfo.nickname !== 'undefined') {
+          await getUserByExternalId(userInfo);
         }
-      })
-      .catch((e) => {
-        console.log(e);
+      },
+      onFailed: () => {
         Alert.alert('잘못된 로그인입니다.');
+      },
+    });
+  };
+
+  const moveToMainScreenWithAnimation = () => {
+    // 로그인 버튼을 포함한 컨테이너를 안보이게
+    Animated.timing(introContainerOpacity, {
+      toValue: 0,
+      duration: 500,
+      useNativeDriver: true,
+    }).start(() => {
+      // 환영 로티를 포함한 컨테이너를 보이게
+      Animated.timing(welcomeContainerOpacity, {
+        toValue: 1,
+        duration: 2000,
+        useNativeDriver: true,
+      }).start(() => {
+        // opacity 타이밍이 끝난 후 바로 이동되는 것은 부자연스러워,
+        // setTimeout 1초 후 로그인 (이동)
+        setTimeout(() => {
+          userStore.setIsLoggedIn(true);
+        }, 1000);
       });
+    });
   };
 
   return (
     <Container source={require('assets/images/login_background.png')}>
-      <Header>
-        <HeaderTitleContainer>
-          <HeaderTitleText>OSORI</HeaderTitleText>
-          <HeaderSubTitleText>
-            <HeaderSubTitleHighlightText>오</HeaderSubTitleHighlightText>
-            늘의 나를 남기는 <HeaderSubTitleHighlightText>소리</HeaderSubTitleHighlightText>
-          </HeaderSubTitleText>
-        </HeaderTitleContainer>
+      <IntroContainer style={{ opacity: introContainerOpacity }}>
+        <Header>
+          <HeaderTitleContainer>
+            <HeaderTitleText>OSORI</HeaderTitleText>
+            <HeaderSubTitleText>
+              <HeaderSubTitleHighlightText>오</HeaderSubTitleHighlightText>
+              늘의 나를 남기는 <HeaderSubTitleHighlightText>소리</HeaderSubTitleHighlightText>
+            </HeaderSubTitleText>
+          </HeaderTitleContainer>
+        </Header>
+        <LoginButtonContainer>
+          <KakaoButton onPress={handleLoginByKakao}>
+            <KakaoImage
+              resizeMode="contain"
+              source={require('assets/images/kakao_login/ko/kakao_login_large_narrow.png')}
+            />
+          </KakaoButton>
+        </LoginButtonContainer>
+      </IntroContainer>
+      <WelcomeContainer style={{ opacity: welcomeContainerOpacity }}>
         <WelcomeText>환영합니다</WelcomeText>
-      </Header>
-      <WelcomeImage
-        resizeMode="cover"
-        source={require('assets/lottie/welcome.json')}
-        speed={0.5}
-        autoPlay
-        loop
-      />
-      <LoginButtonContainer>
-        <KakaoButton onPress={handleLoginByKakao}>
-          <KakaoImage
-            resizeMode="contain"
-            source={require('assets/images/kakao_login/ko/kakao_login_large_narrow.png')}
-          />
-        </KakaoButton>
-      </LoginButtonContainer>
+        <WelcomeImage
+          resizeMode="cover"
+          source={require('assets/lottie/welcome.json')}
+          speed={0.5}
+          autoPlay
+          loop
+        />
+      </WelcomeContainer>
     </Container>
   );
 });
@@ -114,8 +127,13 @@ const Container = styled.ImageBackground`
   align-items: center;
 `;
 
+const IntroContainer = styled(Animated.View)`
+  flex: 1;
+  justify-content: center;
+  z-index: 1;
+`;
+
 const Header = styled.View`
-  height: 40%;
   align-items: center;
   justify-content: center;
 `;
@@ -139,8 +157,8 @@ const HeaderSubTitleHighlightText = styled(Text)`
 `;
 
 const WelcomeText = styled(Text)`
-  font-size: 18px;
-  color: ${(props) => props.theme.colors.label};
+  font-size: 26px;
+  color: ${(props) => props.theme.colors.primary1};
 `;
 
 const LoginButtonContainer = styled.View`
@@ -151,6 +169,12 @@ const KakaoButton = styled.TouchableOpacity``;
 
 const KakaoImage = styled.Image`
   width: ${DEVICE_WIDTH / 2}px;
+`;
+
+const WelcomeContainer = styled(Animated.View)`
+  position: absolute;
+  top: ${DEVICE_HEIGHT / 3}px;
+  align-items: center;
 `;
 
 const WelcomeImage = styled(LottieView)`
--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -1,8 +1,12 @@
 import { makeAutoObservable } from 'mobx';
+import { User } from '../services/User';
 
 export default class UserStore {
+  api: User;
+
   constructor() {
     makeAutoObservable(this);
+    this.api = new User();
   }
 
   isLoggedIn = false;
