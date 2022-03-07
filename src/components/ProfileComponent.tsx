d9efe255171fa2e2c1d6c336b26cece30683fc2b
bongbong2da <snk3981@gmail.com>
2022-03-08 01:37:40 +0900

    [Style] Apply styled-component to ProfileComponent.tsx

--- src/components/ProfileComponent.tsx
+++ src/components/ProfileComponent.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect } from 'react';
-import { Alert, StyleProp, TextStyle, TouchableOpacity, View, ViewStyle } from 'react-native';
+import { Alert, StyleProp, TextStyle, ViewStyle } from 'react-native';
 import { Avatar } from '@ui-kitten/components';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { observer } from 'mobx-react';
+import styled from 'styled-components/native';
 import Text from './Text';
 import { UserDto } from '../services/data-contracts';
 import { useStore } from '../stores/RootStore';
@@ -13,82 +14,70 @@ interface Props {
   textStyle?: StyleProp<TextStyle>;
   userInfo?: UserDto;
   width?: number;
-  showLogout?: boolean;
 }
 
-const ProfileComponent = observer(
-  ({ containerStyle, textStyle, userInfo, width, showLogout }: Props) => {
-    const { userStore } = useStore();
-    const navigation = useNavigation<StackNavigationProp<any>>();
+const ProfileComponent = observer(({ containerStyle, textStyle, userInfo, width }: Props) => {
+  const { userStore } = useStore();
+  const navigation = useNavigation<StackNavigationProp<any>>();
 
-    const handleGoProfileScreen = () => {
-      if (typeof userInfo !== 'undefined') {
-        userStore.setUser(userInfo);
-        navigation.push('ProfileViewScreen');
-      }
-    };
+  const handleGoProfileScreen = () => {
+    if (typeof userInfo !== 'undefined') {
+      userStore.setUser(userInfo);
+      navigation.push('ProfileViewScreen');
+    }
+  };
 
-    const handleLogout = async () => {
-      Alert.alert('로그아웃 하시겠습니까?', '', [
-        {
-          text: '예',
-          onPress: handleLogout,
-        },
-        {
-          text: '아니오',
-        },
-      ]);
-    };
-    useEffect(() => {
-      if (typeof userInfo !== 'undefined') {
-        console.log('user changed', userInfo);
-        console.log('user Image', userInfo?.loginType);
-      }
-    }, [userInfo]);
+  const handleLogout = async () => {
+    Alert.alert('로그아웃 하시겠습니까?', '', [
+      {
+        text: '예',
+        onPress: handleLogout,
+      },
+      {
+        text: '아니오',
+      },
+    ]);
+  };
+  useEffect(() => {
+    if (typeof userInfo !== 'undefined') {
+      console.log('user changed', userInfo);
+      console.log('user Image', userInfo?.loginType);
+    }
+  }, [userInfo]);
 
-    return (
-      <View
-        style={[
-          {
-            paddingVertical: 5,
-            flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'center',
-            // backgroundColor: 'rgba(204,204,204,0.39)',
-            // borderRadius: 12,
-            width: width || 200,
-          },
-          containerStyle,
-        ]}
-      >
-        <TouchableOpacity
-          style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}
-          onPress={handleGoProfileScreen}
-        >
-          <Avatar
-            size="small"
-            source={
-              // typeof userInfo?.profileImg === 'undefined'
-              //   ? require('assets/images/anonymous_user.png')
-              { uri: userInfo?.profileImage }
-            }
-            style={{
-              marginRight: 10,
-              borderWidth: typeof userInfo?.profileImage === 'undefined' ? 0.2 : 0,
-            }}
-          />
-          <Text category="h6" style={textStyle}>
-            {userInfo?.nickname}
-          </Text>
-        </TouchableOpacity>
-        {showLogout && (
-          <TouchableOpacity style={{ marginLeft: 10 }} onPress={handleLogout}>
-            <Text>로그아웃</Text>
-          </TouchableOpacity>
-        )}
-      </View>
-    );
-  },
-);
+  return (
+    <Container style={containerStyle} width={width}>
+      <AvatarContainer onPress={handleGoProfileScreen}>
+        <AvatarImage
+          size="small"
+          source={
+            typeof userInfo?.profileImage === null
+              ? require('assets/images/anonymous_user.png')
+              : { uri: userInfo?.profileImage }
+          }
+        />
+        <Text category="h6" style={textStyle}>
+          {userInfo?.nickname}
+        </Text>
+      </AvatarContainer>
+    </Container>
+  );
+});
+
+const Container = styled.View<{ width?: number }>`
+  flex-direction: row;
+  align-items: center;
+  width: ${(props) => (typeof props.width !== 'undefined' ? props.width : 200)}px;
+`;
+
+const AvatarContainer = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const AvatarImage = styled(Avatar)`
+  margin: 0 10px 0 0;
+`;
 
 export default ProfileComponent;
